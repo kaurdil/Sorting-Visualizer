44d5fd3525f3c313ff3b75a4e82aa9b9a6697705
kaurdil <kaurdilpreet116@gmail.com>
2021-09-26 01:37:51 +0530

    updated main.js

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ arraySize.addEventListener('input',function(){
 
 let delay=260;
 let delayElement = document.querySelector("#speed_input")
+
 // event listener to update delay time
 delayElement.addEventListener('input',function(){
     console.log(delay+" "+delayElement.value);
@@ -16,14 +17,14 @@ delayElement.addEventListener('input',function(){
 
 
 createArray();
+
+// creating Array of bars
 function createArray(nbars=50){
     deletebars();
     array=[];
     for(let i=0;i<nbars;i++){
         array.push(Math.floor(Math.random()*250)+1);
     }
-    //console.log(array);
-
    const bars=document.querySelector("#bars")
    for(var i=0;i<nbars;i++){
        var bar=document.createElement("div")
@@ -40,6 +41,8 @@ function deletebars(){
     bar.innerHTML='';
 }
 
+
+// swapping
 function swap(el1, el2) {
     console.log('In swap()');
     
@@ -49,12 +52,14 @@ function swap(el1, el2) {
     
 }
 var count_var=0;
+
+// comparisons
 function update_comparisons(count_var){
     document.getElementById("count").innerHTML = count_var;
 }
 
 
-
+// function for delaying loop
 function waitforme(milisec) {
     return new Promise(resolve => {
         setTimeout(() => { resolve('') }, milisec);
